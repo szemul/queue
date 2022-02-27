52c474d691d6b73d7452d77989371f439c5e3be2
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2022-02-27 19:10:02 +1300

    Add basic MessageInterface implementation

---
+++ src/Message/Message.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Szemul\Queue\Message;
+
+class Message implements MessageInterface
+{
+    /**
+     * @param mixed[] $payload
+     */
+    public function __construct(
+        protected string $jobName,
+        protected array $payload,
+        protected ?string $queueIdentifier = null
+    ) {
+    }
+
+    public function getPayload(): array
+    {
+        return $this->payload;
+    }
+
+    public function getJobName(): ?string
+    {
+        return $this->jobName;
+    }
+
+    public function getQueueIdentifier(): ?string
+    {
+        return $this->queueIdentifier;
+    }
+
+    public function setQueueIdentifier(string $identifier): static
+    {
+        $this->queueIdentifier = $identifier;
+
+        return $this;
+    }
+}
